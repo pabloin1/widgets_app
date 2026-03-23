5d598e43c203493544884bc927c0f5aff1368df2
pabloin1 <pablocesaraltuzar04@gmail,com>
2026-03-23 01:22:52 -0500

    debugbanner_false

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Center(child: Text('hola')));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: Center(child: Text('hola'))),
+    );
   }
 }
